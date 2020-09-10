db98a465cb77f6c9e14a9570e46d3b2f0d86ead9
Federico Salinas <federicosalinas@Federicos-MacBook-Air.local>
2020-09-09 19:40:07 -0500

    section 3 done, hw will be on another project

--- react_redux_hooks_app/src/App.js
+++ react_redux_hooks_app/src/App.js
@@ -11,25 +11,51 @@ class App extends Component {
   ],
   otherState: 'some other value'
 }
-switchNameHandler = () => {
+switchNameHandler = (newName) => {
     // console.log('was clicked');
     // this.state.persons[0].name = 'Maximilian';
     this.setState({
       persons: [
-        {name: 'Maximilian', age: 28},
+        {name: newName, age: 28},
         {name: 'Manu', age: 29},
         {name: 'Stephanie', age: 27}
       ]
     } )
   };
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: 'Max', age: 28},
+        {name: event.target.value, age: 29},
+        {name: 'Stephanie', age: 26}
+      ]
+    } )
+  }
   render(){
+    const style = {
+      backGroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
-      <button onClick={this.switchNameHandler}>Switch Name</button>
-      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      <button 
+        style={style}
+        onClick={() => this.switchNameHandler('Maximilian!!!')}>Switch Name</button>
+      <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age}/>
+      <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age}
+        click={this.switchNameHandler.bind(this, 'Max!')}
+        changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
+      <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age}/>
     </div>
     // React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'I am a React App!'))
   );
--- react_redux_hooks_app/src/Person/Person.js
+++ react_redux_hooks_app/src/Person/Person.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import '../Person/Person.css'
 
 const person = (props) => {
     return (
-        <div>
-            <p>I'm {props.name} and I am {props.age} years old!</p>
+        <div className='Person'>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
+            {/* two way binding.. listen to changes and  */}
+            <input type='text' onChange={props.changed} value={props.name}/>
         </div>
     )
 };
