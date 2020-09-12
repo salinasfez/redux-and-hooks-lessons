3b1be06da40c9ba6bb24d6b2aaf5928f62f42e2e
Federico Salinas <federicosalinas@Federicos-MacBook-Air.local>
2020-09-12 14:12:34 -0500

    section 4 unit 56, rendering lists

--- react_redux_hooks_app/src/App.js
+++ react_redux_hooks_app/src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
     {name: 'Manu', age: 29},
     {name: 'Stephanie', age: 26}
   ],
-  otherState: 'some other value'
+  otherState: 'some other value',
+  showPersons: false
 }
 switchNameHandler = (newName) => {
     // console.log('was clicked');
@@ -31,6 +32,12 @@ switchNameHandler = (newName) => {
       ]
     } )
   }
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShow
+    })
+  }
   render(){
     const style = {
       backGroundColor: 'white',
@@ -39,23 +46,30 @@ switchNameHandler = (newName) => {
       padding: '8px',
       cursor: 'pointer'
     };
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+        {this.state.persons.map(person => {
+           return (
+            <Person 
+            name={person.name} 
+            age={person.age}/>
+           )
+        })}
+      </div> 
+      )
+    }
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <button 
         style={style}
-        onClick={() => this.switchNameHandler('Maximilian!!!')}>Switch Name</button>
-      <Person 
-        name={this.state.persons[0].name} 
-        age={this.state.persons[0].age}/>
-      <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age}
-        click={this.switchNameHandler.bind(this, 'Max!')}
-        changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
-      <Person 
-        name={this.state.persons[2].name} 
-        age={this.state.persons[2].age}/>
+        onClick={this.togglePersonsHandler}>Toggle Persons
+      </button>
+      
+      {persons}
+        
     </div>
     // React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'I am a React App!'))
   );
